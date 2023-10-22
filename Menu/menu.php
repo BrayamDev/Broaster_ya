b4d7f7e763f1a54b6442a2fbbb6e71573a221fa8
brayam fajardo <brayamfajardo23@gmail.com>
2023-10-21 22:11:28 -0500

    Realizacion de la parte de menu y responsive

---
+++ Menu/menu.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Icono google-->
+    <link rel="shortcut icon" href="Icons/Logo-Broaster-Ya.ico">
+    <!--css-->
+    <link rel="stylesheet" href="../css/style.css" class="css">
+    <!--Font awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Menu Completo</title>
+</head>
+
+<body>
+    <div class="menu_tittle">
+        <h1>Broaster YA</h1>
+
+        <a href="../index.php">Regresar</a>
+
+    </div>
+    <div class="menu_subtittle">
+        <h1><i class="fa-solid fa-bowl-food"></i> Menu</h1>
+    </div>
+    <br>
+    <div class="grid">
+        <div class="menu__food">
+            <div>
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/1.jpg" alt="imagen comida">
+            </div>
+            <p class="menu__food-titulo">Hamburguesa de salmon</p>
+            <p class="menu__food-precio">$15.000</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+
+        </div>
+        <div class="menu__food">
+            <div>
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/2.jpg" alt="imagen comida">
+            </div>
+            <p class="menu__food-titulo">Hamburguesa especial</p>
+            <p class="menu__food-precio">$1.000</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+
+        </div>
+        <div class="menu__food">
+            <div>
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/3.jpg" alt="imagen comida">
+            </div>
+            <p class="menu__food-titulo">Mini pizzas</p>
+            <p class="menu__food-precio">$150.000</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+        </div>
+        <div class="menu__food">
+            <div class="menu__food-container">
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/4.jpg" alt="imagen comida">
+            </div>
+            <p class="menu__food-titulo">Mini pizza de salmon</p>
+            <p class="menu__food-precio">$50.000</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+        </div>
+        <div class="menu__food">
+
+            <div>
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/5.jpg" alt="imagen comida">
+            </div>
+            <p class="menu__food-titulo">Salchichon</p>
+            <p class="menu__food-precio">$5.000</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+        </div>
+        <div class="menu__food">
+
+            <div>
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/6.jpg" alt="imagen comida">
+            </div>
+            <p class="menu__food-titulo">Tacos de birria</p>
+            <p class="menu__food-precio">$60.000</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+
+        </div>
+        <div class="menu__food">
+            <div>
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/7.jpg" alt="imagen comida">
+            </div>
+            <p class="menu__food-titulo">Tacos de carne</p>
+            <p class="menu__food-precio">$70.000</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+        </div>
+        <div class="menu__food">
+            <div>
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/8.jpg" alt="imagen comida">
+            </div>
+            <p class="menu__food-titulo">Pan con queso</p>
+            <p class="menu__food-precio">$800.000</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+        </div>
+        <div class="menu__food">
+
+            <div>
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/9.jpg" alt="imagen comida">
+            </div>
+            <p class="menu__food-titulo">Salmon con yogurth</p>
+            <p class="menu__food-precio">$90.520</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+        </div>
+        <div class="menu__food">
+
+            <div>
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/10.jpg" alt="imagen comida">
+            </div>
+
+            <p class="menu__food-titulo">Salmon con yogurth envasado</p>
+            <p class="menu__food-precio">$10.120</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+        </div>
+        <div class="menu__food">
+            <div>
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/11.jpg" alt="imagen comida">
+            </div>
+            <p class="menu__food-titulo">Spaguetti</p>
+            <p class="menu__food-precio">$11.120</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+        </div>
+        <div class="menu__food">
+            <div>
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/12.jpg" alt="imagen comida">
+            </div>
+            <p class="menu__food-titulo">Hamburguesa de salmon</p>
+            <p class="menu__food-precio">$15.200</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+        </div>
+        <div class="menu__food">
+
+            <div>
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/13.jpg" alt="imagen comida">
+            </div>
+            <p class="menu__food-titulo">Arroz chino</p>
+            <p class="menu__food-precio">$850.200</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+        </div>
+        <div class="menu__food">
+
+            <div>
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/14.jpg" alt="imagen comida">
+            </div>
+            <p class="menu__food-titulo">Perro caliente</p>
+            <p class="menu__food-precio">$10.200</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+        </div>
+        <div class="menu__food">
+            <div>
+                <img class="menu__food-imagen" src="/images/imgenes_comidas/15.jpg" alt="imagen comida">
+            </div>
+            <p class="menu__food-titulo">Salchipapa costeña</p>
+            <p class="menu__food-precio">$70.300</p>
+            <div class="menu__food-buttons">
+                <a href=""> <i class="fa-solid fa-list-check"></i> Pedir</a>
+                <a href=""> <i class="fa-solid fa-list-check"></i> Ver mas</a>
+                <a href=""> <i class="fa-solid fa-cart-plus"></i> Añadir al carrito</a>
+            </div>
+        </div>
+    </div>
+    <br>
+    <!--footer-->
+    <section class="footer_main">
+        <div class="footer">&copy;<span id="year"> </span><span>BROASTER YA!, Todos los derechos reservados .</span></div>
+    </section>
+
+</body>
+
+</html>
